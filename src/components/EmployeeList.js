29195c3e601b2d12a08156b035636c862372893a
shiba_1234 <shibajisahu121@gmail.com>
2023-09-15 03:11:09 +0530

    list page structure done

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function EmployeeList() {
+  const [employeList, setEmployeList] = useState([]);
+  useEffect(() => {
+    fetch(" https://sweede.app/DeliveryBoy/Get-Employee/")
+      .then((response) => response.json())
+      .then((employeList) => setEmployeList(employeList))
+      .catch((error) => console.log(error));
+  }, [employeList]);
+
   return (
-    <div>
+    <div className="employee-list">
       <h1>EmployeeList</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>DOB</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employeList.map((employe) => {
+            return (
+              <tr key={employe.id}>
+                <td>
+                  {employe.FirstName} {employe.LastName}
+                </td>
+                <td>{employe.DOB}</td>
+                <td>{employe.StartDate}</td>
+                <td>{employe.EndDate}</td>
+                <td>{employe.Description}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
